e0676a7e537bf7f7b56fc86cab924591376d8d80
alex6 <alex66651@yandex.ru>
2024-02-25 18:38:55 +0800

    Дима

--- lesson_20_1.py
+++ lesson_20_1.py
@@ -1,7 +1,9 @@
-'lesson 20_1'
+"lesson 20_1"
+"lesson 20_2"
 def Hello_name(name):
     print(f'Hello, {name}!')
 Hello_name('Вася')
 Hello_name("Петя")
 Hello_name("Саша")
 Hello_name("Андрей")
+Hello_name("Дима")
